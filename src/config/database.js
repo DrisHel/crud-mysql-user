8aa2c129eebb5f3465975274f58a2b3166e17c87
Doris Nunes <doris90nunes@gmail.com>
2022-02-16 17:29:56 -0300

    feat: criação da pasta config e arquivo de configuração com banco de dados.

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "mySql2",
+  host: "localhost",
+  usarname: "root",
+  password: "123456",
+  database: "bd_mysql",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
